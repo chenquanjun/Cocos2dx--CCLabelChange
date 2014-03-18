9c6fcd2dbe83c095bac3e5eb66dc29e2df8f0e8d
chenquanjun <chenshiyouxiang@gmail.com>
2014-03-18 15:02:43 +0800

    first commit

---
+++ testLabelChange/CCLabelChange.cpp
@@ -0,0 +1,9 @@
+//
+//  CCLabelChange.cpp
+//  testLabelChange
+//
+//  Created by chenquanjun on 14-1-23.
+//
+//
+
+#include "CCLabelChange.h"
---
+++ testLabelChange/CCLabelChange.h
@@ -0,0 +1,14 @@
+//
+//  CCLabelChange.h
+//  testLabelChange
+//
+//  Created by chenquanjun on 14-1-23.
+//
+//
+
+#ifndef __testLabelChange__CCLabelChange__
+#define __testLabelChange__CCLabelChange__
+
+#include <iostream>
+
+#endif /* defined(__testLabelChange__CCLabelChange__) */
